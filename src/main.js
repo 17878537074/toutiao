ebf2bced7f4fa8154ea83971aa5fe9778d7f3edb
马垚谟 <1085482669@qq.com>
2020-03-28 00:01:30 +0800

    个人中心设置路由守卫

--- src/main.js
+++ src/main.js
@@ -9,7 +9,23 @@ Vue.prototype.$axios = axios;
 
 axios.defaults.baseURL = "http://localhost:3000"
 Vue.use(Vant);
+// 添加路由的守卫
+router.beforeEach((to, from, next) => {
+  // to表示即将进入的页面
+  // from表示即将要离开的页面
+  // next必须要调用   next类似于node.js的中间件
+  if (to.path === "/personal") {
+    const userjson = JSON.parse(localStorage.getItem("userInfo")) || {}
+    if (userjson.token) {
+      next();
+    } else {
+      next("/login");
+    }
+  } else {
+    next();
 
+  }
+})
 Vue.config.productionTip = false
 
 new Vue({
