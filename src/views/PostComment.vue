5ce8a819768fbfda6e3098b7343a41bc1c2965b7
马垚谟 <1085482669@qq.com>
2020-04-11 00:49:54 +0800

    评论页的分页

--- src/views/PostComment.vue
+++ src/views/PostComment.vue
@@ -1,21 +1,24 @@
 <template>
   <div>
     <Navigatebar title="精彩跟帖" :showHome="true"></Navigatebar>
-    <div class="comment" v-for="(item,index) in list" :key="index">
-      <div class="comment-top">
-        <div class="user">
-          <img :src="$axios.defaults.baseURL + item.user.head_img" alt />
-          <div class="user-info">
-            <p>{{item.user.nickname}}</p>
-            <span>{{moment(item.create_date).fromNow()}}</span>
+    <!-- v-model是否正在加载中 finished是否加载完毕 @load滚动到底部加载的事件-->
+    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad" :immediate-check="false">
+      <div class="comment" v-for="(item,index) in list" :key="index">
+        <div class="comment-top">
+          <div class="user">
+            <img :src="$axios.defaults.baseURL + item.user.head_img" alt />
+            <div class="user-info">
+              <p>{{item.user.nickname}}</p>
+              <span>{{moment(item.create_date).fromNow()}}</span>
+            </div>
           </div>
+          <span class="reply">回复</span>
         </div>
-        <span class="reply">回复</span>
+        <!-- 回复组件 -->
+        <commentFloor v-if="item.parent" :data="item.parent"></commentFloor>
+        <div class="content">{{item.content}}</div>
       </div>
-      <!-- 回复组件 -->
-      <commentFloor v-if="item.parent" :data="item.parent"></commentFloor>
-      <div class="content">{{item.content}}</div>
-    </div>
+    </van-list>
   </div>
 </template>
 
@@ -23,15 +26,20 @@
 import Navigatebar from "@/components/Navigatebar";
 import commentFloor from "@/components/commentFloor";
 // 日期转换的库
-import moment from "moment"
+import moment from "moment";
+import { log } from 'util';
 moment.locale("zh-CN");
 
 export default {
   data() {
     return {
-      pid:"",
-      list:[],
-      moment
+      pid: "",
+      list: [],
+      moment,
+      loading:false,
+      finished:false,
+      pageIndex:1,
+      pageSize:5
     };
   },
   components: {
@@ -39,7 +47,7 @@ export default {
     commentFloor
   },
   mounted() {
-    const{id} = this.$route.params;
+    const { id } = this.$route.params;
     this.pid = id;
     // console.log(id);
     this.getist();
@@ -47,13 +55,25 @@ export default {
   methods: {
     getist() {
       this.$axios({
-        url: `/post_comment/${this.pid}`
+        url: `/post_comment/${this.pid}`,
+        params:{
+          pageIndex:this.pageIndex,
+          pageSize:this.pageSize
+        }
       }).then(res => {
         console.log(res);
-        const {data} =res.data;
-       
-        this.list=data;
+        const { data } = res.data;
+        this.list =[...this.list,...data];
+        this.pageIndex+=1;
+        this.loading=false;
+        if(data.length<this.pageSize){
+       this.finished=true;
+        }
       });
+    },
+    onLoad(){
+      // console.log(11);
+      this.getist();
     }
   }
 };
@@ -62,7 +82,7 @@ export default {
 <style lang="less" scoped>
 @px: 360/100vw;
 .comment {
-  padding: 15 / @px;
+  padding: 20 / @px 15 / @px;
   border-bottom: 1px solid #eee;
   .content {
     margin-top: 10 / @px;
@@ -72,6 +92,7 @@ export default {
   display: flex;
   justify-content: space-between;
   margin-bottom: 20 / @px;
+  padding: 10/@px 5px;
   .user {
     display: flex;
     align-items: center;
